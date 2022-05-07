941f52fe12f43d4c599fd9f9ee5410271b215eb3
TheLorexxx <104691158+TheLorexxx@users.noreply.github.com>
2022-05-07 03:01:31 +0300

    Create fake_data.py

---
+++ fake_data.py
@@ -0,0 +1,29 @@
+import time
+import random
+
+data = ['a' , 'b' , 'c' , 'd' , 'e' , 'f' , 'g' ,'i' , 'h' , 'j' , 'k', 'l' , 'm' , 'n' , 'o' , 'p' , 'q' , 'r' , 's' , 't' , 'u' , 'v' , 'w' , 'x' , 'y' , 'z' , '1' , '2' , '3' , '4' , '5' , '6' , '7' , '8' , '9' , '0']
+print("Program Lorex ve Rootman tarafindan yapilmistir.")
+print("")
+print("")
+data1 = random.choice(data)
+data2 = random.choice(data)
+data3 = random.choice(data)
+data4 = random.choice(data)
+data5 = random.choice(data)
+data6 = random.choice(data)
+data7 = random.choice(data)
+data8 = random.choice(data)
+print("Username : ")
+print(data1+data2+data3+data4+data5+data6+data7+data8)
+datap1 = random.choice(data)
+datap2 = random.choice(data)
+datap3 = random.choice(data)
+datap4 = random.choice(data)
+datap5 = random.choice(data)
+datap6 = random.choice(data)
+datap7 = random.choice(data)
+datap8 = random.choice(data)
+print("Password : ")
+print(datap1+datap2+datap3+datap4+datap5+datap6+datap7+datap8)
+
+time.sleep(9999)
